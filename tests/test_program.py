d54c533b8b2be6d185f5bc40ba2660ac7679994a
dmytro-serhiichuk <157469257+DmytroSerhiichuk@users.noreply.github.com>
2024-04-05 11:18:32 +0300

    added test for filtering lines

--- tests/test_program.py
+++ tests/test_program.py
@@ -12,4 +12,18 @@ def test_read_lines(prepered_file):
                  'last end stop\n']
     assert read_lines(prepered_file) == expected
 
-
+@pytest.mark.parametrize('lines, keyword, result', [
+    ([
+        'word key blue\n',
+        'red green white\n',
+        'rat word text\n',
+        'hello world end\n'
+    ],
+    'word',
+    [
+        'word key blue\n',
+        'rat word text\n'
+    ])
+])
+def test_filter_lines(lines, keyword, result):
+    assert filter_lines(lines, keyword) == result
