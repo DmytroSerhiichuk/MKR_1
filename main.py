8f61ee83486def89af0a52caa9bb98b7a3f77735
dmytro-serhiichuk <157469257+DmytroSerhiichuk@users.noreply.github.com>
2024-04-05 10:55:37 +0300

    added filter lines func

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ def read_lines(file_name: str) -> list[str]:
     with open(file_name, 'r') as file:
         return file.readlines()
 
+def filter_lines(lines: list[str], keyword: str) -> list[str]:
+    return [line for line in lines if keyword in line]
 
 def main():
     file_name = 'data.txt'
 
     lines = read_lines(file_name)
 
+    filtered_lines = filter_lines(lines, 'world')
 
 if __name__ == '__main__':
     main()
