96d03b8496f05b36d60a18b72ff4be7ed825b3d2
dmytro-serhiichuk <157469257+DmytroSerhiichuk@users.noreply.github.com>
2024-04-05 11:40:50 +0300

    added docstrings

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 
 def read_lines(file_name: str) -> list[str]:
+    '''
+    Reads lines from file
+
+    Params:
+    - file_name: the name of the file from which the data is read
+
+    Returns:
+    - list[str]: read lines
+    '''
     with open(file_name, 'r') as file:
         return file.readlines()
 
 def filter_lines(lines: list[str], keyword: str) -> list[str]:
+    '''
+    Filters lines by keyword
+
+    Params:
+    - lines: lines that will be filtered
+    - keyword
+
+    Returns:
+    - list[str]: filtered lines 
+    '''
     return [line for line in lines if keyword in line]
 
 def save_lines(lines: list[str], output_file_name: str):
+    '''
+    Save lines to the file
+
+    Params:
+    - lines: lines that will be saved
+    - output_file_name: the path to the file to which the lines will be written
+    '''
     with open(output_file_name, 'w') as file:
         for line in lines:
             file.write(line)
