f1684a30ec42467f1d36b1d1b968aad58918555b
dmytro-serhiichuk <157469257+DmytroSerhiichuk@users.noreply.github.com>
2024-04-05 10:58:18 +0300

    added save filtered line func

--- main.py
+++ main.py
@@ -6,12 +6,19 @@ def read_lines(file_name: str) -> list[str]:
 def filter_lines(lines: list[str], keyword: str) -> list[str]:
     return [line for line in lines if keyword in line]
 
+def save_lines(lines: list[str], output_file_name: str):
+    with open(output_file_name, 'w') as file:
+        for line in lines:
+            file.write(line)
+
 def main():
     file_name = 'data.txt'
 
     lines = read_lines(file_name)
 
     filtered_lines = filter_lines(lines, 'world')
+
+    save_lines(filtered_lines, 'filtered.txt')
 
 if __name__ == '__main__':
     main()
